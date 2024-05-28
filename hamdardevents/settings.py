3184a3bfd798ea672c583ca595ef253cb9f48a3d
Hassan Aziz <hassanaziz0012@gmail.com>
2024-05-28 12:58:07 +0500

    Debugging

--- hamdardevents/settings.py
+++ hamdardevents/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-ilxc381x^0m(bg4vep)1m5vaph90f18h$tu8^j!krn77o!f@71
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 
 # Application definition
