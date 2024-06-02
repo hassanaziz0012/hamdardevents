ab24dc1be078d626509428a874aa931b1c7abc39
Hassan Aziz <hassanaziz0012@gmail.com>
2024-06-02 09:44:57 +0500

    Add event managers

--- events/admin.py
+++ events/admin.py
@@ -5,8 +5,12 @@ from events.models import Event
 # Register your models here.
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ["name", "price", "event_date", "_participants"]
+    list_display = ["name", "price", "event_date", "_managers", "_participants"]
+
+    def _managers(self, event):
+        return event.display_managers()
 
     def _participants(self, event):
         return event.participants.count()
+        
 
--- events/models.py
+++ events/models.py
@@ -9,7 +9,11 @@ class Event(models.Model):
     creation_date = models.DateTimeField(default=timezone.now)
     event_date = models.DateTimeField()
 
-    participants = models.ManyToManyField('users.Member', blank=True)
+    participants = models.ManyToManyField('users.Member', blank=True, related_name='event_participants')
+    managers = models.ManyToManyField('users.Member', blank=True, related_name='event_managers')
+
+    def display_managers(self):
+        return ', '.join(list(self.managers.all().values_list('user__username', flat=True)))
 
     def __str__(self) -> str:
         return self.name
